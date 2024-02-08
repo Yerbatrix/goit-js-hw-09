126b06ff342ec26dae3d9225d15a1508c03e2f93
Rafał Pobóg-Ruszkowski <ruszkowski.pobog@gmail.com>
2024-02-08 12:57:33 +0100

    task-01-partoff

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+const background = document.body;
 
+startButton.addEventListener('click', () => {
+  const changeContinue = setInterval(changeColor, 1000);
+});
+
+function changeColor() {
+  const color = getRandomHexColor();
+  background.style.backgroundColor = color;
+  startButton.disabled = true;
+}
+
+stopButton.addEventListener('click', stopChange);
+
+function stopChange() {
+  console.log('Stop Button has been clicked');
+  clearInterval(changeContinue);
+  startButton.disabled = false;
+}
