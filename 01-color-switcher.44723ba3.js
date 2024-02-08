f9a42f7568f44501b30afbdad6d7fea13118d19d
Yerbatrix <142529173+Yerbatrix@users.noreply.github.com>
2024-02-08 14:17:01 +0000

    Deploying to gh-pages from @ Yerbatrix/goit-js-hw-09@70fc9f89b74d24f1fb3c72b3815d2501e70c1a97 🚀

---
+++ 01-color-switcher.44723ba3.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),a=document.body;let d=null;t.addEventListener("click",(()=>{d=setInterval((()=>{const e=`#${Math.floor(16777215*Math.random()).toString(16)}`;a.style.backgroundColor=e,t.disabled=!0}),1e3)})),e.addEventListener("click",(()=>{clearInterval(d),t.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.44723ba3.js.map
---
+++ 01-color-switcher.56b3fd31.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=document.body,a=null;t.addEventListener("click",(function(){a=setInterval((function(){var e="#".concat(Math.floor(16777215*Math.random()).toString(16));n.style.backgroundColor=e,t.disabled=!0}),1e3)})),e.addEventListener("click",(function(){clearInterval(a),t.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.56b3fd31.js.map
