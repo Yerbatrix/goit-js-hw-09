d9b952522efa690c85e6c1a2016abed26e604957
Rafał Pobóg-Ruszkowski <ruszkowski.pobog@gmail.com>
2024-02-10 16:26:18 +0100

    task-02-part2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,6 +2,15 @@ import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 const datetimePicker = document.querySelector('#datetime-picker');
 
+const startButton = document.querySelector('[data-start]');
+let timerDays = document.querySelector('[data-days]');
+let timerHours = document.querySelector('[data-hours]');
+let timerMinutes = document.querySelector('[data-minutes]');
+let timerSeconds = document.querySelector('[data-seconds]');
+let selectedDate = '';
+let currentDate = '';
+let countdownInterval;
+
 flatpickr(datetimePicker, {
   enableTime: true,
   time_24hr: true,
@@ -9,5 +18,13 @@ flatpickr(datetimePicker, {
   minuteIncrement: 1,
   onClose(selectedDates) {
     console.log(selectedDates[0]);
+    currentDate = new Date();
+    selectedDate = new Date(selectedDates[0]);
+    if (selectedDate < currentDate) {
+      window.alert('Please choose a date in the future');
+      startButton.setAttribute('disabled', true);
+    } else {
+      startButton.removeAttribute('disabled');
+    }
   },
 });
