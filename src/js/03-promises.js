514e48ec68bbd170f4a6606fb38a0382545193d4
Rafał Pobóg-Ruszkowski <ruszkowski.pobog@gmail.com>
2024-02-11 10:30:35 +0100

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,6 +1,9 @@
 const form = document.querySelector('.form');
+const delay = form.elements.delay.value;
+const step = form.elements.step.value;
+const amount = form.elements.amount.value;
 
-const delay = function createPromise(position, delay) {
+function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       const shouldResolve = Math.random() > 0.3;
@@ -11,4 +14,18 @@ const delay = function createPromise(position, delay) {
       }
     }, delay);
   });
-};
+}
+
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+  for (let i = 1; i <= amount; i += 1) {
+    let furtherDelay = delay + (i - 1) * step;
+    createPromise(i, furtherDelayDelay)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+  }
+});
