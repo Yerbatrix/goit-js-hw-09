69a19ab7c5476d71c8c922ae4223276ae5ab7ac9
Yerbatrix <142529173+Yerbatrix@users.noreply.github.com>
2024-02-11 09:12:01 +0000

    Deploying to gh-pages from @ Yerbatrix/goit-js-hw-09@8673398457a7e61786979144086a86e1a60f78e8 🚀

---
+++ 03-promises.7017709c.js
@@ -0,0 +1,2 @@
+document.querySelector(".form");
+//# sourceMappingURL=03-promises.7017709c.js.map
---
+++ 03-promises.89280063.js
@@ -0,0 +1,2 @@
+document.querySelector(".form");
+//# sourceMappingURL=03-promises.89280063.js.map
